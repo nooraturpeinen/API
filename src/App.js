6251bf14c5ec1a92f2df770e01b93a389fe13a88
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-02-13 21:05:33 +0200

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const URL = "https://api.lyrics.ovh/v1";
 
 function App() {
+  const [artist, setArtist] = useState("");
+  const [title, setTitle] = useState("");
+  const [lyrics, setLyrics] = useState("");
+
+  async function getLyrics(e) {
+    e.preventDefault();
+    try {
+      const address = URL + "/" + artist + "/" + title;
+      const response = await fetch(address);
+      if (response.ok) {
+        const json = await response.json();
+        const lyrics = json.lyrics;
+        const correctedLyrics = lyrics.substring(22);
+        const finalLyrics = correctedLyrics.replace("par", "by");
+        if (lyrics.substring(0, 22) === "Paroles de la chanson ") {
+          setLyrics(finalLyrics);
+        } else {
+          setLyrics(lyrics);
+        }
+      } else {
+        alert("Lyrics not found.");
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={getLyrics}>
+        <div>
+          <h2>Song Lyrics</h2><hr/>
+        </div>
+        <div style={{marginTop: "25px", marginBottom: "10px"}}>
+          <label for="artist">Artist:</label>
+          <input value={artist} onChange={e => setArtist(e.target.value)}/>
+        </div>
+        <div style={{marginTop: "15px", marginBottom: "10px"}}>
+          <label for="title">Song Title:</label>
+          <input value={title} onChange={e => setTitle(e.target.value)}/>
+        </div>
+        <div style={{marginTop: "20px", marginBottom: "35px"}}>
+          <button>Show Lyrics</button>
+        </div>
+        <div style={{whiteSpace: "pre-wrap", marginTop: "25px", marginBottom: "30px"}}>
+          <p>{lyrics}</p>
+        </div>
+      </form>
     </div>
   );
 }
